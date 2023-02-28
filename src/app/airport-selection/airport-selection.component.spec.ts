fd7463096589257daac5f2b7b71fe3c6cff16227
dselmer <dselmer@icstars.org>
2023-02-28 13:48:09 -0600

    changed login btn color

---
+++ src/app/airport-selection/airport-selection.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AirportSelectionComponent } from './airport-selection.component';
+
+describe('AirportSelectionComponent', () => {
+  let component: AirportSelectionComponent;
+  let fixture: ComponentFixture<AirportSelectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AirportSelectionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AirportSelectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/airport-selection/airport-selection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-airport-selection',
+  templateUrl: './airport-selection.component.html',
+  styleUrls: ['./airport-selection.component.css']
+})
+export class AirportSelectionComponent {
+
+}
