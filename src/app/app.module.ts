c3dd4f381186c20aea08a736474a68df84cbc237
dselmer <122752508+dselmer@users.noreply.github.com>
2023-03-08 14:42:04 -0600

    sign up form completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ReportFormComponent } from './report-form/report-form.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ReportFormComponent
+    ReportFormComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
